6e6e029fc8d2db5d54276f183ed5810339db297a
samyog437 <fallendemon174@gmail.com>
2021-08-25 18:10:45 +0545

    added sound

--- game.py
+++ game.py
@@ -1,9 +1,61 @@
-import pygame
+import csv
+import os
+import random
+import sqlite3
+
+from bar import *
+from button import Button
 
 pygame.init()
+pygame.font.init()
 
-running = True
+clock = pygame.time.Clock()
+fps = 60
+keys = pygame.key.get_pressed()
+
+turn_left = False
+turn_right = False
+
+font = pygame.font.SysFont("comicsansms", 20)
+fon2 = pygame.font.SysFont("comicsansms", 30)
+
+pygame.display.set_caption("Runnin")
+
+sun_img = pygame.image.load('img/sun.png')
+sun_img = pygame.transform.scale(sun_img, (tile_size, tile_size))
+bg_img = pygame.image.load('img/sky.png').convert_alpha()
+bg_img = pygame.transform.scale(bg_img, (width, height))
+pine1_img = pygame.image.load('img/pine1.png').convert_alpha()
+pine2_img = pygame.image.load('img/pine2.png').convert_alpha()
+mountain_img = pygame.image.load('img/mountain.png').convert_alpha()
+sky_img = pygame.image.load('img/sky_cloud.png').convert_alpha()
+
+restart_img = pygame.image.load('img/restart_btn.png').convert_alpha()
+exit_img = pygame.image.load('img/exit_btn.png').convert_alpha()
+start_img = pygame.image.load('img/start_btn.png').convert_alpha()
+save_img = pygame.image.load('img/save_btn.png').convert_alpha()
+
+score_coin_img = pygame.image.load('img/tile/32.png').convert_alpha()
+coin_img = pygame.transform.scale(score_coin_img, (25, 25))
+
+bullet_count = pygame.transform.scale(bullet_img, (25, 25))
+
+nameActive = False
+newName = ""
+user = ""
+
+passActive = False
+newPass = ""
+passw = ""
+
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+
 while running:
+    draw_text()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
